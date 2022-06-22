9ad03b413df8df5d47048972125052551956050f
Jacob Solomon <jcbslmn@Jacobs-iMac.modem>
2022-06-22 17:48:32 +1000

    feat: footer

---
+++ src/pages/privacyPolicy.tsx
@@ -0,0 +1,10 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const PrivacyPolicy = () => (
+  <Main meta={<Meta title="Commercial 1 GC" description="Commercial 1 GC" />}>
+    PrivacyPolicy!
+  </Main>
+);
+
+export default PrivacyPolicy;
