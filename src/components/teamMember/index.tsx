c5453cf0260e807364104a0bfc860639f2ff3f85
Jacob Solomon <jcbslmn@Jacobs-iMac.modem>
2022-07-05 15:05:50 +1000

    feat: agent info pages

---
+++ src/components/teamMember/index.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+
+import type { IAgent } from '@/types';
+
+const TeamMember = ({ agent }: { agent: IAgent }) => {
+  return (
+    <div className="space-y-4">
+      <div className="aspect-w-3 aspect-h-2">
+        <Link href={`/agents/${agent.slug}`}>
+          <img
+            className="rounded-lg object-cover shadow-lg transition duration-500 hover:scale-105 hover:cursor-pointer "
+            src={agent.imageUrl}
+            alt="agent image"
+          />
+        </Link>
+      </div>
+
+      <div className="space-y-2">
+        <div className="space-y-1 text-lg font-medium leading-6 hover:cursor-pointer hover:text-orange-300">
+          <Link href={`/agents/${agent.slug}`}>
+            <h3>{agent.name}</h3>
+          </Link>
+          <p className="text-orange-600">{agent.role}</p>
+        </div>
+
+        <ul role="list" className="flex space-x-5">
+          {agent.mobile ? (
+            <li>
+              {' '}
+              <a href={`tel: ${agent.mobile}`}>M. {agent.mobile}</a>
+            </li>
+          ) : (
+            <li></li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMember;
---
+++ src/constants.ts
@@ -0,0 +1,53 @@
+import type { IAgent } from '@/types';
+
+export const AGENTS: IAgent[] = [
+  {
+    id: 1,
+    name: 'Steve Macgregor',
+    slug: 'steve-macgregor',
+    role: 'Managing Director | Sales & Leasing',
+    imageUrl: '/assets/images/Steve.jpg',
+    mobile: '0410679664',
+    about:
+      'Not only am I a proud Gold Coast resident, but I am also a passionate believer in the areas property market, particularly the commercial sector.<br/><br/>Prior to becoming a real estate agent I ran a family manufacturing business with both national and international contracts. Through this the company became involved in owning and renting many commercial properties while also carrying out major renovation projects.<br/><br/>This experience was a terrific grounding to embark on a career in commercial and industrial property and over the past eleven years I have sold and leased in excess of 3 billion dollars worth of real estate.<br/><br/>As director of Commercial 1 Gold Coast my focus is on achieving the very best results and outcomes for our clients combined with a more personalised service.',
+  },
+  {
+    id: 2,
+    name: 'Adam Quinn',
+    slug: 'adam-quinn',
+    role: 'Leasing & Sales Executive',
+    imageUrl: '/assets/images/Adam.jpg',
+    mobile: '0400371989',
+    about:
+      'Adam’s decision to enter the Commercial Real Estate industry followed on from the success the family business had already achieved and the expertise he could add to compliment Steve, Ryan & the team already in place.<br/><br/>After living on the Gold Coast from a young age and following on from his previous career with a background in engineering and drafting, Adam has a passion, extensive knowledge and experience around commercial and industrial buildings the property sector in general.<br/><br/>Adam is committed to providing the highest level of communication to his clients which has proven to be a recipe for success in the past.',
+  },
+  {
+    id: 3,
+    name: 'Ryan Macgregor',
+    slug: 'ryan-macgregor',
+    role: 'Leasing & Sales Executive',
+    imageUrl: '/assets/images/Ryan.jpg',
+    mobile: '0406226533',
+    about:
+      'Ryan Macgregor is a driven individual who has a passion for service. <br/><br/> Born and raised on the Gold Coast, Ryan became a young entrepreneur in 2012 owing and operating a sign company for 5 years at which point Ryan decided it was time for a change and wanted to follow in his fathers footsteps becoming a commercial and industrial leasing and sales specialist. <br/><br/> During Ryan’s time in the real estate industry, he has consistently achieved great results and is considered a hard working, youthful agent by his customers. Ryan prides himself on leaving no stone unturned, following up and providing the very best service to both owners and tenants. <br/><br/> With the resources available to him by both Steve Macgregor and Commercial 1 Gold Coast, he will without a doubt continue to succeed.',
+  },
+  {
+    id: 4,
+    name: 'Mark Kelada',
+    slug: 'mark-kelada',
+    role: 'Auctioneer',
+    imageUrl: '/assets/images/Mark.jpg',
+    mobile: '0410679664',
+    about:
+      'My investment property portfolio began in 1991 having purchased and flipped my first property on the Gold Coast at age 19. I have since purchased/sold around 20 properties both residential and industrial to date and still own a number of industrial and residential properties on the Gold Coast.<br/><br/>I completed my studies as an auctioneer with REIQ in 2008 and was immediately offered and signed on as a provisional auctioneer by my REIQ trainer which was an absolute honour and privilege to be so highly regarded by my lecturer. I obtained my full auctioneers licence in 2009 and started my auction career as a Luxury boat auctioneer, and soon thereafter progressed into automotive, chattel and above all investment and high end Real Estate in 2010.<br/><br/>I have successfully auctioned in excess of 1 billion dollars worth of Residential, Commercial and Industrial property and continue to invest in the place I love and believe in most of all being the Gold Coast.<br/><br/>(My Beginnings) I was born and raised in Melbourne and brought up in the property industry as my father was a renowned architect in Melbourne and is best known for his design of the iconic Melbourne Arts Centre Spire Tower. He developed many commercial and residential properties in Melbourne from 1971 till the time we moved to the Gold Coast in 1987 and then continued to develop on the Gold Coast in the residential property market for both first home buyers as well as luxury waterfront and commercial property. He was an active property investor on the Gold Coast having developed around 100 properties in total, and at almost age 81 continues to passionately practice architecture to date.<br/><br/>Real Estate is not a simple transaction for me, it is a passion and a way of life! It’s an industry I “live, breath, love and embrace” on a daily basis, no challenge is too hard and my focus will always be creative marketing and sales whilst achieving outstanding results for our clients.',
+  },
+  {
+    id: 5,
+    name: 'Katie Rossi',
+    slug: 'katie-rossi',
+    role: 'Sales and Marketing Administrator',
+    imageUrl: '/assets/images/Katie.jpg',
+    about:
+      'They say if you want to learn something, then learn from the best, which is exactly what Katie Rossi set out to do when she joined her father Steve Macgregor in commercial real estate in 2008.<br/><br/>A typical gen Y, Katie has a great deal of IT knowledge and is constantly looking for better and improved ways of marketing property whether it be for sale or lease.<br/><br/>One might think that Commercial / Industrial Sales and Leasing is more of a “blokie” thing however coming from a close knit family that is centered around building, development and team work, Katie well understands the wants and needs and certainly knows her way around a factory floor.<br/><br/>Katie Rossi is a young lady heading for the top, so who better for you to have on your side for all your Commercial and Industrial property needs.',
+  },
+];
