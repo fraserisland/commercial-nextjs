111e6570fa7ca3b12483f3c0277884f805213516
fraserisland <fmsolomon1@gmail.com>
2022-07-06 15:04:22 +1000

    eslint fix

--- src/components/search/ActiveFilters.tsx
+++ src/components/search/ActiveFilters.tsx
@@ -1,7 +1,4 @@
-import {
-  useClearRefinements,
-  useCurrentRefinements,
-} from 'react-instantsearch-hooks-web';
+import { useClearRefinements, useCurrentRefinements } from 'react-instantsearch-hooks-web';
 
 import capitalise from '@/utils/capitalise';
 
@@ -12,15 +9,13 @@ const getPrefix = (attribute: string) => {
   return name;
 };
 
-const getFormattedNumberLabel = (label: string, attribute) => {
+const getFormattedNumberLabel = (label: string, attribute: string) => {
   const isPrice = attribute === 'price';
   const isSqm = attribute === 'floorArea';
   const num = label.split(' ');
   const formatted = parseInt(num[1] as string, 2).toLocaleString();
 
-  return `${num[0]?.replace('≥', 'over').replace('≤', 'under')} ${
-    isPrice ? '$' : ''
-  }${formatted}${isSqm ? 'sqm' : ''}`;
+  return `${num[0]?.replace('≥', 'over').replace('≤', 'under')} ${isPrice ? '$' : ''}${formatted}${isSqm ? 'sqm' : ''}`;
 };
 
 const transformItems = (items: any) => {
@@ -31,10 +26,7 @@ const transformItems = (items: any) => {
     refinements: item.refinements.map((r: any) => ({
       ...r,
       label: prefixedFilters.includes(r.attribute)
-        ? `${getPrefix(r.attribute)} ${getFormattedNumberLabel(
-            r.label,
-            r.attribute
-          )}`
+        ? `${getPrefix(r.attribute)} ${getFormattedNumberLabel(r.label, r.attribute)}`
         : capitalise(r.label),
     })),
   }));
@@ -47,50 +39,36 @@ const ActiveFilters = () => {
   });
 
   return (
-    <div className="bg-gray-100">
-      <div className="mx-auto max-w-7xl py-3 px-4 sm:flex sm:items-center sm:px-6 lg:px-8">
-        <h3 className="nowrap py-1 text-xs font-semibold uppercase tracking-wide text-gray-500">
+    <div className='bg-gray-100'>
+      <div className='mx-auto max-w-7xl py-3 px-4 sm:flex sm:items-center sm:px-6 lg:px-8'>
+        <h3 className='nowrap py-1 text-xs font-semibold uppercase tracking-wide text-gray-500'>
           APPLIED FILTERS
-          <span className="sr-only">, active</span>
+          <span className='sr-only'>, active</span>
         </h3>
 
-        <div
-          aria-hidden="true"
-          className="hidden h-5 w-px bg-gray-300 sm:ml-4 sm:block"
-        />
+        <div aria-hidden='true' className='hidden h-5 w-px bg-gray-300 sm:ml-4 sm:block' />
 
-        <div className="mt-2 sm:mt-0 sm:ml-4">
-          <div className="-m-1 flex flex-wrap items-center">
+        <div className='mt-2 sm:mt-0 sm:ml-4'>
+          <div className='-m-1 flex flex-wrap items-center'>
             {items.map((item) => (
               <div key={item.label}>
                 {item.refinements.map((itemRefinement) => {
                   return (
                     <span
                       key={item.attribute + itemRefinement.label}
-                      className="m-1 inline-flex items-center rounded-full border border-gray-200 bg-white pl-3 pr-2 text-sm font-medium text-gray-900"
+                      className='m-1 inline-flex items-center rounded-full border border-gray-200 bg-white pl-3 pr-2 text-sm font-medium text-gray-900'
                     >
                       <span>{itemRefinement.label}</span>
                       <button
-                        type="button"
-                        className="ml-1 inline-flex h-4 w-4 shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-200 hover:text-gray-500"
+                        type='button'
+                        className='ml-1 inline-flex h-4 w-4 shrink-0 rounded-full p-1 text-gray-400 hover:bg-gray-200 hover:text-gray-500'
                         onClick={() => {
                           refine(itemRefinement);
                         }}
                       >
-                        <span className="sr-only">
-                          Remove filter for {item.label}
-                        </span>
-                        <svg
-                          className="h-2 w-2"
-                          stroke="currentColor"
-                          fill="none"
-                          viewBox="0 0 8 8"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeWidth="1.5"
-                            d="M1 1l6 6m0-6L1 7"
-                          />
+                        <span className='sr-only'>Remove filter for {item.label}</span>
+                        <svg className='h-2 w-2' stroke='currentColor' fill='none' viewBox='0 0 8 8'>
+                          <path strokeLinecap='round' strokeWidth='1.5' d='M1 1l6 6m0-6L1 7' />
                         </svg>
                       </button>
                     </span>
@@ -103,7 +81,7 @@ const ActiveFilters = () => {
         {canRefine && (
           <button
             style={{ marginLeft: 'auto' }}
-            className="nowrap py-1 text-xs font-semibold tracking-wide"
+            className='nowrap py-1 text-xs font-semibold tracking-wide'
             onClick={clearRefinements}
           >
             clear all
