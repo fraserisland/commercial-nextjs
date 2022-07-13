4447748e1063c3d99b78366ba958818107032faf
fraserisland <fmsolomon1@gmail.com>
2022-07-13 13:16:32 +1000

    named export

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,7 +1,7 @@
-export default function () {
-  return (
-    <div>
-      <hr className='my-6 border-orange-100' />
-    </div>
-  );
-}
+const Divider = () => (
+  <div>
+    <hr className='my-6 border-orange-100' />
+  </div>
+);
+
+export default Divider;
